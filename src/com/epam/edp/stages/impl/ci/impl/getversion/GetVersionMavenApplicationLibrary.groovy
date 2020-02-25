e9ba0fefbf238c05df3a5a86359dbcc06847e445
Ihor Kalyniak <Ihor_Kalyniak@epam.com>
2020-02-25 17:04:06 +0200

    [EPMDEDP-4574] Fixed setting basic version for releases branches
    Change-Id: I4d1c49cb9e676f555e02ab5a874f7cab33b829cc

--- src/com/epam/edp/stages/impl/ci/impl/getversion/GetVersionMavenApplicationLibrary.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/getversion/GetVersionMavenApplicationLibrary.groovy
@@ -22,12 +22,13 @@ import com.epam.edp.stages.impl.ci.Stage
 class GetVersionMavenApplicationLibrary {
     Script script
     def setVersionToArtifact(buildNumber, context) {
+       def startFrom = context.platform.getJsonPathValue("codebasebranches.v2.edp.epam.com", "${context.codebase.config.name}-${context.git.branch}", ".spec.version")
        def newBuildNumber = ++buildNumber
        script.sh """
-            sed -i "0,/<version>.*<\\/version>/s/<version>.*<\\/version>/<version>${context.codebase.config.startFrom}-${newBuildNumber}<\\/version>/" pom.xml
+            sed -i "0,/<version>.*<\\/version>/s/<version>.*<\\/version>/<version>${startFrom}-${newBuildNumber}<\\/version>/" pom.xml
         """
 
-       return "${context.codebase.config.startFrom}-${newBuildNumber}"
+       return "${startFrom}-${newBuildNumber}"
     }
 
     def updateCodebaseBranchCR(buildNumber, context) {
@@ -42,10 +43,13 @@ class GetVersionMavenApplicationLibrary {
             script.withCredentials([script.usernamePassword(credentialsId: "${context.nexus.credentialsId}",
                     passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
                 if (context.codebase.config.versioningType == "edp") {
-                    context.codebase.version = setVersionToArtifact(context.codebase.config.codebase_branch.build_number.get(0).toInteger(), context)
-                    context.codebase.buildVersion = "${context.codebase.version}"
+                    def build = context.platform.getJsonPathValue("codebasebranches.v2.edp.epam.com", "${context.codebase.config.name}-${context.git.branch}", ".spec.build")
+                    script.println("codebase_branch map (branchName): ")
+                    script.println(context.codebase.config.codebase_branch.get('branchName')
 
-                    updateCodebaseBranchCR(context.codebase.config.codebase_branch.build_number.get(0).toInteger(), context)
+                    context.codebase.version = setVersionToArtifact(build, context)
+                    context.codebase.buildVersion = "${context.codebase.version}"
+                    updateCodebaseBranchCR(build, context)
                  } else {
                     context.codebase.version = script.sh(
                             script: """
