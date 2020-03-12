654ec2e4362193bf6af62480012498106b328daf
Ihor Kalyniak <Ihor_Kalyniak@epam.com>
2020-03-12 14:57:47 +0200

    Verbose log
    Change-Id: I8b11e9b5f678a467554490ee59e241ef2e680666

--- src/com/epam/edp/stages/impl/ci/impl/getversion/GetVersionMavenApplicationLibrary.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/getversion/GetVersionMavenApplicationLibrary.groovy
@@ -25,8 +25,15 @@ class GetVersionMavenApplicationLibrary {
     def setVersionToArtifact(context) {
         script.sh """
             set -eo pipefail
+<<<<<<< HEAD
             sed -i "0,/<version>.*<\\/version>/s/<version>.*<\\/version>/<version>${context.codebase.branchVersion}-${context.codebase.currentBuildNumber}<\\/version>/" pom.xml
             kubectl patch codebasebranches.v2.edp.epam.com ${context.codebase.config.name}-${context.git.branch} --type=merge -p '{\"spec\": {\"build\": "${context.codebase.currentBuildNumber}"}}'
+=======
+            sed -i "0,/<version>.*<\\/version>/s/<version>.*<\\/version>/<version>${branchVersion}-${newBuildNumber}<\\/version>/" pom.xml
+            kubectl get codebasebranches.v2.edp.epam.com ${context.codebase.config.name}-${context.git.branch} -o yaml
+            kubectl patch codebasebranches.v2.edp.epam.com ${context.codebase.config.name}-${context.git.branch} --type=merge -p '{\"spec\": {\"build\": "${newBuildNumber}"}}'
+            kubectl get codebasebranches.v2.edp.epam.com ${context.codebase.config.name}-${context.git.branch} -o yaml
+>>>>>>> Verbose log
         """
     }
 
