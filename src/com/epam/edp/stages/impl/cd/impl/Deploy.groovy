b639595493ff97377e1d7b456fcbbe462f92f8fe
Mykola_Marusenko <mykola_marusenko@epam.com>
2020-03-19 10:20:34 +0200

    [EPMDEDP-4809] Adapt tags in VCS according to pattern: build/1.7.0-RC.8
    Change-Id: Ibd514ab7c947bd28c2e9f66a5b9dda345323f55b

--- src/com/epam/edp/stages/impl/cd/impl/Deploy.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/Deploy.groovy
@@ -143,14 +143,15 @@ class Deploy {
         script.println("[JENKINS][DEBUG] Repository path: ${repoPath}")
 
         def gitCodebaseUrl = "ssh://${autouser}@${host}:${sshPort}${repoPath}"
+        def prefix = "build"
 
         try {
-            script.checkout([$class                           : 'GitSCM', branches: [[name: "refs/tags/${codebase.version}"]],
+            script.checkout([$class                           : 'GitSCM', branches: [[name: "refs/tags/${prefix}/${codebase.version}"]],
                              doGenerateSubmoduleConfigurations: false, extensions: [],
                              submoduleCfg                     : [],
                              userRemoteConfigs                : [[credentialsId: "${credentialsId}",
-                                                                  refspec      : "refs/tags/${codebase.version}",
-                                                                  url          : "${gitCodebaseUrl}"]]])
+                                                                      refspec      : "refs/tags/${prefix}/${codebase.version}",
+                                                                      url          : "${gitCodebaseUrl}"]]])
         }
         catch (Exception ex) {
             script.unstable("[JENKINS][WARNING] Project ${codebase.name} cloning has failed with ${ex}\r\n" +
