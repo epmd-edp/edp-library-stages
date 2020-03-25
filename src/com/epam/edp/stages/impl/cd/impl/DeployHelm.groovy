90f728740ad39c879d98b064c435e6227a105d7a
Mykola_Marusenko <mykola_marusenko@epam.com>
2020-03-25 12:26:44 +0200

    [4786] Refactor Deploy Helm stage for Helm 3
    Change-Id: I9ec508ce77233afd9b165c00011af6b590e0045b

--- src/com/epam/edp/stages/impl/cd/impl/DeployHelm.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/DeployHelm.groovy
@@ -143,11 +143,11 @@ class DeployHelm {
         def gitCodebaseUrl = "ssh://${autouser}@${host}:${sshPort}${repoPath}"
 
         try {
-            script.checkout([$class                           : 'GitSCM', branches: [[name: "refs/tags/${codebase.version}"]],
+            script.checkout([$class                           : 'GitSCM', branches: [[name: "refs/tags/build/0.0.1-snapshot-8 "]],
                              doGenerateSubmoduleConfigurations: false, extensions: [],
                              submoduleCfg                     : [],
                              userRemoteConfigs                : [[credentialsId: "${credentialsId}",
-                                                                  refspec      : "refs/tags/${codebase.version}",
+                                                                  refspec      : "refs/tags/build/0.0.1-snapshot-8 ",
                                                                   url          : "${gitCodebaseUrl}"]]])
         }
         catch (Exception ex) {
@@ -187,7 +187,7 @@ class DeployHelm {
                 "${deployTemplatesPath}",
                 "${context.environment.config.dockerRegistryHost}/${imageName}",
                 codebase, context.job.dnsWildcard,
-                "300",
+                "300s",
                 context.platform.verifyDeployedCodebase(codebase.name, context.job.deployProject)
         )
     }
