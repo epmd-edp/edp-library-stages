ca6d36e0d0cb0ae0227db4d40331dd0b0e9ee780
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-04-27 14:05:13 +0300

    [EPMDEDP-5031] Implement loading custom stages from user's jenkins shared library
    Change-Id: Ibf84421da72af14ecc7e0b3e13d5f155ea6968d7

--- src/com/epam/edp/stages/StageFactory.groovy
+++ src/com/epam/edp/stages/StageFactory.groovy
@@ -39,6 +39,34 @@ class StageFactory {
         return classesList
     }
 
+    @NonCPS
+    def loadCustomStagesFromLib() {
+        def classesList = []
+        def res = Thread.currentThread().getContextClassLoader().getResources("com/epam/edp/customStages/impl")
+
+        try {
+            def dir = new File(res.nextElement().getFile())
+            dir.eachDirRecurse() { directory ->
+            directory.eachFile(FileType.FILES) { file ->
+                classesList.push(Class.forName("com.epam.edp.customStages.impl.${directory.path.replace("${dir.path}/", "").replaceAll('/', '.')}."
+                        + file.name.substring(0, file.name.length() - 7)))
+                }
+            }
+            if (classesList.contains(null)) {
+            classesList.toList().each { i -> 
+                if (i == null) {
+                    classesList.remove(i)
+                    }
+                }
+                script.println("[JENKINS][DEBUG] One or more files from custom stage are empty")
+            }
+        } catch (java.util.NoSuchElementException ex) {
+            script.println("[JENKINS][DEBUG] Not found custom stages from lib")
+        }
+
+        return classesList
+    }
+
     def loadCustomStages(String directory) {
         def classesList = []
         def customStagesDir
@@ -53,6 +81,10 @@ class StageFactory {
         customStagesDir.list().each {
             classesList.push(script.load(it.getRemote()))
         }
+        script.println("V: ${classesList}")
+        classesList.contains(null) ? classesList.removeAll([null])
+
+        script.println("VV: ${classesList}")
         return classesList
     }
 
