4d02aa1df385dfa746bd2461aa3029899c30fb43
Mykola_Marusenko <mykola_marusenko@epam.com>
2020-05-19 18:05:37 +0300

    [EPMDEDP-5110] Fix sonar stages with import strategy
    Change-Id: Ibc58a91fec9142183112e514128899d860a20727

--- src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
@@ -73,7 +73,8 @@ class SonarMaven {
         }
     }
     def runSonarScannerDependsOnPlatform(context, platform, codereviewAnalysisRunDir) {
-        if (platform == "kubernetes") {
+        script.println("[JENKINS][WARNING] STRATEGY ${context.codebase.config.strategy}")
+        if (platform == "kubernetes" || context.codebase.config.strategy == "import") {
             sendSonarScan(context.codebase.name, codereviewAnalysisRunDir, context.buildTool, context.nexus.credentialsId)
         } else {
             sendSonarScan("${context.codebase.name}:change-${context.git.changeNumber}-${context.git.patchsetNumber}", codereviewAnalysisRunDir, context.buildTool, context.nexus.credentialsId)
