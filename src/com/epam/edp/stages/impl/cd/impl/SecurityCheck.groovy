b63f041607ec7fe3488d5ad4ebd756a42b55378b
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-06-25 12:07:21 +0300

    Add security check
    Change-Id: I4e0d6da157eb386da42688921b6dd42c0d192aec

---
+++ src/com/epam/edp/stages/impl/cd/impl/SecurityCheck.groovy
@@ -0,0 +1,21 @@
+package com.epam.edp.stages.impl.cd.impl
+
+import com.epam.edp.stages.impl.cd.Stage
+
+@Stage(name = "security-check")
+class SecurityCheck {
+    Script script
+    void run(context) {
+        def codebasesList = context.job.codebasesList
+        codebasesList.each() { codebase -> 
+            def options = "${context.job.optionsForSecurityCheck} ${codebase.name}"
+            script.dir("${context.workDir}") {
+                script.withCredentials([script.string(credentialsId: "${context.job.stageName}.url", variable: "serverUrl")]) {
+                    script.withKubeConfig([credentialsId: "${context.job.stageName}.token", serverUrl: "${script.serverUrl}"]) { 
+                        context.platform.runPod(context.job.deployProject, context.job.imageForSecurityCheck, context.job.securityCheckName, options)
+                    }
+                }
+            }
+        }
+    }
+}
