655407de3983d865c6c89a87a20960ecbfff3039
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-09-03 13:04:02 +0300

    Add Helm Version
    Change-Id: Ice22d68015a83db4b40a5e7356fb1660d685357f

--- src/com/epam/edp/stages/impl/ci/impl/helmverify/HelmVerifyApplicationLibrary.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/helmverify/HelmVerifyApplicationLibrary.groovy
@@ -11,9 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 See the License for the specific language governing permissions and
 limitations under the License.*/
-
 package com.epam.edp.stages.impl.ci.impl.helmverify
-
 
 import com.epam.edp.stages.impl.ci.ProjectType
 import com.epam.edp.stages.impl.ci.Stage
@@ -21,16 +19,16 @@ import com.epam.edp.stages.impl.ci.Stage
 @Stage(name = "helm-verify", buildTool = ["gradle", "maven", "dotnet", "npm"], type = [ProjectType.APPLICATION, ProjectType.LIBRARY])
 class HelmVerifyApplicationLibrary {
     Script script
-
     void run(context) {
+        def helmDownloadUrl = context.job.getParameterValue("HELM_DOWNLOAD_URL", "https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz")
         script.dir("${context.workDir}") {
             script.sh """
-            V=\$(curl -Ls https://github.com/helm/helm/releases | grep 'href="/helm/helm/releases/tag/v3.' | grep -v no-underline | head -n 1 | cut -d '"' -f 2 | awk '{n=split(\$NF,a,"/");print a[n]}' | awk 'a !~ \$0{print}; {a=\$0}')
-            curl -o helm.tar.gz https://get.helm.sh/helm-\${V}-linux-amd64.tar.gz
+            curl -o helm.tar.gz ${helmDownloadUrl}
             tar -xzf helm.tar.gz linux-amd64/helm --strip 1
             values_files=`find ${context.job.deployTemplatesDirectory} -maxdepth 1 -name 'values*.yaml'`
             for file in \${values_files}; do echo Linting with \${file} values; ./helm lint "${context.job.deployTemplatesDirectory}" --values \${file}; done
             """
             }
         }
+
     }
