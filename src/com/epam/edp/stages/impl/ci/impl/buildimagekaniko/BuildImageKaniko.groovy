bab8aeaedd6e3721dbb746bff1747ee3a96c00fa
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-09-25 12:52:23 +0300

    dockerfile1
    Change-Id: I1fe68a3fa308045022c672426c7709c2197baadd

--- src/com/epam/edp/stages/impl/ci/impl/buildimagekaniko/BuildImageKaniko.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/buildimagekaniko/BuildImageKaniko.groovy
@@ -85,8 +85,16 @@ class BuildImageKaniko {
                 def deployableModuleDirFilepath = new FilePath(Jenkins.getInstance().getComputer(script.env['NODE_NAME']).getChannel(), "${context.workDir}")
                 script.println("[JENKINS][DEBUG] Files to copy to kaniko - ${deployableModuleDirFilepath.list()}")
                 deployableModuleDirFilepath.list().each() { item ->
-                    if (item.getName() != "Dockerfile")
-                        context.platform.copyToPod("${context.workDir}/${item.getName()}", "/tmp/workspace/", buildconfigName, null, "init-kaniko")
+                    if (item.getName() == "Dockerfile") {
+                        return
+                    }
+
+                    if (context.codebase.build_tool == "maven" && item.getName() == "target") {
+                        script.println("[JENKINS][DEBUG] JAVA MAVEN ${context.workDir}/${item.getName()}/*.jar")
+                        context.platform.copyToPod("${context.workDir}/${item.getName()}/*.jar", "/tmp/workspace/", buildconfigName, null, "init-kaniko")
+                    }
+
+                    context.platform.copyToPod("${context.workDir}/${item.getName()}", "/tmp/workspace/", buildconfigName, null, "init-kaniko")
                 }
                 context.platform.copyToPod("Dockerfile", "/tmp/workspace", buildconfigName, null, "init-kaniko")
 
