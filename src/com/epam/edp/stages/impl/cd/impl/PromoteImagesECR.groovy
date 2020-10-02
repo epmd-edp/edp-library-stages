7a08c9170f8ca3298fb82e38d4ce81ed652bacd6
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-10-02 16:53:14 +0300

    [EPMDEDP-5437]: Fix name of ECR
    Change-Id: I06013a841d0eee44fb6763dda315fe1c2afa35e0

--- src/com/epam/edp/stages/impl/cd/impl/PromoteImagesECR.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/PromoteImagesECR.groovy
@@ -33,6 +33,7 @@ class PromoteImagesECR {
     }
 
     def setKanikoTemplate(outputFilePath, buildPodName, resultImageName, dockerRegistryHost, context, codebase) {
+        script.println("[JENKINS][DEBUG] setKanikoTemplate ${resultImageName}")
         def kanikoFile = new File(outputFilePath)
         def kanikoTemplateFilePath = new FilePath(kanikoFile)
 
@@ -76,13 +77,14 @@ class PromoteImagesECR {
                 def sourceImageName = "${codebase.inputIs}:${codebase.version}"
                 def buildconfigName = "promote-${codebase.outputIs}-${script.BUILD_NUMBER}"
 
-                def dockerfileContents = "FROM ${dockerRegistryHost}/${sourceImageName}"
+                def dockerfileContents = "FROM ${dockerRegistryHost}/${context.job.ciProject}/${sourceImageName}"
                 def dockerfilePath = new FilePath(new File("${context.workDir}/Dockerfile"))
                 dockerfilePath.write(dockerfileContents, null)
 
                 script.dir("${context.workDir}") {
                     try {
-                        def kanikoTemplateFilePath = setKanikoTemplate("${context.workDir}/kaniko-template.json", buildconfigName, codebase.outputIs, dockerRegistryHost, context, codebase)
+                        def kanikoTemplateFilePath = setKanikoTemplate("${context.workDir}/kaniko-template.json", buildconfigName,
+                                "${context.job.ciProject}/${codebase.outputIs}", dockerRegistryHost, context, codebase)
                         context.platform.apply(kanikoTemplateFilePath.getRemote())
                         while (!context.platform.getObjectStatus("pod", buildconfigName)["initContainerStatuses"][0].state.keySet().contains("running")) {
                             script.println("[JENKINS][DEBUG] Waiting for init container in Kaniko is started")
@@ -101,7 +103,7 @@ class PromoteImagesECR {
                         script.println("[JENKINS][DEBUG] Promote ${buildconfigName} for application ${codebase.name} has been completed")
 
                         new CodebaseImageStreams(context, script)
-                                .UpdateOrCreateCodebaseImageStream(codebase.outputIs, "${dockerRegistryHost}/${codebase.outputIs}", codebase.version)
+                                .UpdateOrCreateCodebaseImageStream(codebase.outputIs, "${dockerRegistryHost}/${context.job.ciProject}/${codebase.outputIs}", codebase.version)
                     }
                     catch (Exception ex) {
                         script.println("[JENKINS][ERROR] Trace: ${ex.getStackTrace().collect { it.toString() }.join('\n')}")
